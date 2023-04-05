984f2e27f2881d49dc43f84d64136c0025840b84
ch <mkmsacc@outlook.com>
2023-04-05 13:57:43 +0700

    Implement sse test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
+	@Autowired
+	private WebTestClient client;
+
 	@Test
-	void contextLoads() {
+	void shouldRetrieveSSE() {
+		var flux = client.get()
+				.uri("/subscribe")
+				.exchange()
+				.expectStatus()
+				.is2xxSuccessful()
+				.returnResult(String.class)
+				.getResponseBody()
+				.log();
+
+		client.get()
+				.uri("/publish/{data}", "Alex")
+				.exchange();
+
+		StepVerifier.create(flux)
+				.expectNext("data", "Alex")
+				.thenCancel()
+				.verify();
+
 	}
 
 }
